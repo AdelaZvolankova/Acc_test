1154df0562e0bb91fdf1a7ed9b39c982e33e58a5
AdelaZvolankova <56041026+AdelaZvolankova@users.noreply.github.com>
2020-07-07 16:04:46 +0200

    Delete task1.py

--- Test/task1.py
+++
@@ -1,9 +0,0 @@
-for num in reversed (range(1,101)):
-  if num % 3 == 0 and num % 5 == 0:
-    print('Testing')
-  elif num % 3 == 0 and num % 5 > 0:
-    print('Software')
-  elif num % 3 > 0 and num % 5 == 0:
-    print('Agile')
-  else:
-    print(num) 
