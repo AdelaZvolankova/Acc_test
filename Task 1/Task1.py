5d622c057843f747a8474afe9aaf9e3fa3960339
AdelaZvolankova <56041026+AdelaZvolankova@users.noreply.github.com>
2020-07-13 10:34:52 +0200

    Update Task1.py

--- Task 1/Task1.py
+++ Task 1/Task1.py
@@ -1,1 +1,9 @@
-
+for num in reversed (range(1,101)):
+  if num % 3 == 0 and num % 5 == 0:
+    print('Testing')
+  elif num % 3 == 0 and num % 5 > 0:
+    print('Software')
+  elif num % 3 > 0 and num % 5 == 0:
+    print('Agile')
+  else:
+    print(num) 
