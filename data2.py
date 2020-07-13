9b0544ad7d95906314924c8f16c50c9652eec4f3
AdelaZvolankova <56041026+AdelaZvolankova@users.noreply.github.com>
2020-07-13 10:24:48 +0200

    Delete data2.py

--- data2.py
+++
@@ -1,6 +0,0 @@
-Case1 = ['Hi Ivan', 'Hi Adela', 'Hi John']
-Case2 = ['Hi Adela', 'Hi Ivan', 'Michal', 'Nod', 'surf', 'John']
-Case3 = {'key1': 'value1', 'key2': 'value2', 'key3': 'value3'}
-expected_value = 'Hi Adela'
-expected_value2 = 'value2'
-value_to_be_removed = 'Hi Ivan'
